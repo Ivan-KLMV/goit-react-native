363b18e0c37b69bbdb73b61a81c738c493aefd4a
Ivan Klimov <klimov128@gmail.com>
2023-06-27 23:33:10 +0300

    Update App.js

--- App.js
+++ App.js
@@ -22,35 +22,12 @@ export default function App() {
           source={require('./img/photo_BG.png')}
           style={{ flex: 1 }}
           resizeMode="cover"
-        ></ImageBackground>
-        <StatusBar style="auto" />
-        <RegistrationScreen />
+        >
+          <StatusBar style="auto" />
+          {/* <RegistrationScreen /> */}
+          <LoginScreen />
+        </ImageBackground>
       </View>
     </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  inner: {
-    padding: 24,
-    flex: 1,
-    justifyContent: 'flex-end',
-  },
-  header: {
-    fontSize: 36,
-    marginBottom: 48,
-  },
-  textInput: {
-    height: 40,
-    borderColor: '#000000',
-    borderBottomWidth: 1,
-    marginBottom: 16,
-  },
-  btnContainer: {
-    backgroundColor: 'white',
-    marginTop: 12,
-  },
-});
