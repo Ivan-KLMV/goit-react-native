c30e87317df0b1d151179992c9368055298bc711
Ivan Klimov <klimov128@gmail.com>
2023-07-24 04:25:08 +0300

    Update HomePage.js

--- Screens/HomePage.js
+++ Screens/HomePage.js
@@ -3,8 +3,8 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import PostScreen from './PostsScreen';
 import CreatePostsScreen from './CreatePostsScreen';
-import LogoutButton from '../components/LogoutButton';
 import ProfileScreen from './ProfileScreen';
+import ExitButton from '../components/ExitButton';
 
 const Tab = createBottomTabNavigator();
 
@@ -15,6 +15,8 @@ const HomePage = (props) => {
     <Tab.Navigator
       initialRouteName="PostScreen"
       screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: { height: 45 },
         headerTitleAlign: 'center',
         headerTitleStyle: {
           fontSize: 17,
@@ -29,7 +31,7 @@ const HomePage = (props) => {
         name="Публікації"
         component={PostScreen}
         options={{
-          headerRight: () => <LogoutButton myProp={loginHandler} />,
+          headerRight: () => <ExitButton myProp={loginHandler} />,
         }}
       />
       <Tab.Screen name="Створити публікацію" component={CreatePostsScreen} />
