8e68190cbb6b0a18c6260432663fe8082b57b053
Ivan Klimov <klimov128@gmail.com>
2023-07-26 13:52:03 +0300

    Create PostCard.js

---
+++ components/PostCard.js
@@ -0,0 +1,52 @@
+import { Image, StyleSheet, Text, View } from 'react-native';
+import ComentMessageSvgComponent from './SVGs/ComentMessageSvgComponent';
+import ThumbsUpSvgComponent from './SVGs/ThumbsUpSvgComponent';
+import MapPinSvgComponent from './SVGs/MapPinSvgComponent';
+
+const PostCard = () => {
+  return (
+    <View
+      style={{
+        height: 240,
+      }}
+    >
+      <Image
+        source={require('../img/forest.jpeg')}
+        resizeMode="contain"
+        style={{ width: '100%', maxHeight: 240, borderRadius: 8 }}
+      />
+      <Text
+        style={{ marginTop: 8, marginBottom: 8, fontSize: 16, fontWeight: 500 }}
+      >
+        Ліс
+      </Text>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+        <View style={{ flexDirection: 'row', gap: 24 }}>
+          <View style={{ flexDirection: 'row' }}>
+            <ComentMessageSvgComponent style={{ marginRight: 8 }} />
+            <Text style={{ fontSize: 16, fontWeight: 400 }}>8</Text>
+          </View>
+          <View style={{ flexDirection: 'row' }}>
+            <ThumbsUpSvgComponent style={{ marginRight: 8 }} />
+            <Text style={{ fontSize: 16, fontWeight: 400 }}>153</Text>
+          </View>
+        </View>
+        <View style={{ flexDirection: 'row' }}>
+          <MapPinSvgComponent style={{ marginRight: 8 }} />
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: 400,
+              textDecorationLine: 'underline',
+            }}
+          >
+            Ukraine
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+export default PostCard;
