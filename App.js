3b6f1af901dd3ad7537df9d39420d603d9e1ec58
Ivan Klimov <klimov128@gmail.com>
2023-09-30 17:18:18 +0300

    Update App.js
    removed all components to the 'src' folder

--- App.js
+++ App.js
@@ -1,56 +1,16 @@
-import { useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './src/redux/store';
 
-import RegistrationScreen from './Screens/RegistrationScreen';
-import LoginScreen from './Screens/LoginScreen';
-// import PostScreen from './Screens/PostsScreen';
-import HomePage from './Screens/HomePage';
-import MapScreen from './Screens/MapScreen';
-import CommentsScreen from './Screens/CommentsScreen';
+import { AppNavigation } from './src/AppNavigation';
+import { Text } from 'react-native';
 
 export default function App() {
-  const [isLogined, setIslogined] = useState(true);
-
-  const loginHandler = (value) => {
-    console.log('loginHandler is pressed, isLogined:', isLogined);
-
-    setIslogined(value);
-  };
-
-  const MainStack = createStackNavigator();
-
   return (
-    <NavigationContainer>
-      {isLogined ? (
-        <MainStack.Navigator>
-          <MainStack.Screen
-            name="HomePage"
-            component={HomePage}
-            options={{
-              headerShown: false,
-            }}
-            initialParams={{ loginHandler }}
-          />
-          <MainStack.Screen name="MapScreen" component={MapScreen} />
-          <MainStack.Screen name="Коментарі" component={CommentsScreen} />
-        </MainStack.Navigator>
-      ) : (
-        <MainStack.Navigator initialRouteName="RegistrationScreen">
-          <MainStack.Screen
-            name="RegistrationScreen"
-            component={RegistrationScreen}
-            options={{ headerShown: false }}
-            initialParams={{ loginHandler }}
-          />
-          <MainStack.Screen
-            name="LoginScreen"
-            component={LoginScreen}
-            options={{ headerShown: false }}
-            initialParams={{ loginHandler }}
-          />
-        </MainStack.Navigator>
-      )}
-    </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate loading={<Text>..Loading</Text>} persistor={persistor}>
+        <AppNavigation />
+      </PersistGate>
+    </Provider>
   );
 }
